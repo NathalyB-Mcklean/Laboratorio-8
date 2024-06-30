7c8af0f709d0a79cfdff9319cffe0c3e13d9f884
Nathaly Bonilla <127335373+NathalyB-Mcklean@users.noreply.github.com>
2024-06-29 23:35:33 -0500

    Por fin, version uwu oficial

---
+++ Abrego, Bonilla, Joseph, Brandford Lab 8/src/grafico/Busqueda.java
@@ -0,0 +1,50 @@
+package grafico;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+
+public class Busqueda extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Busqueda frame = new Busqueda();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Busqueda() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 606, 457);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Búsqueda");
+		lblNewLabel.setBounds(307, 11, 49, 14);
+		contentPane.add(lblNewLabel);
+	}
+}
